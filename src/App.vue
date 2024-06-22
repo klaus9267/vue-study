e2b6766be1fd394bda1c3a059cfc5c8c5576a30f
Kim minho <klaus9267@gmail.com>
2024-06-23 00:05:06 +0900

    16 배열에서 아이템 삭제

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,12 @@
       </div>
       <div v-show="hasError" style="color:red">This field cannot be empty</div>
     </form>
-    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2">
-        <div class="form-check">
+    <div v-if="!todos.length" class="alert alert-danger p-2 mt-2"  >
+      추가된 Todo가 없습니다
+    </div>
+    <div v-for="(todo,index) in todos" :key="todo.id" class="card mt-2">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input class="form-check-input" type="checkbox" v-model="todo.completed">
           <label
               class="form-check-label"
@@ -30,7 +33,14 @@
             {{ todo.subject }}
           </label>
         </div>
-
+        <div>
+          <button
+              class="btn btn-danger btn-sm"
+              @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -68,12 +78,17 @@ export default {
       }
     };
 
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    }
+
     return {
       todo,
       todos,
       onSubmit,
       hasError,
       todoStyle,
+      deleteTodo,
     };
   },
 };
