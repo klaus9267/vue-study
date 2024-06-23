eaf2e674aadb0e56ca4ed215b590052fd9d77523
Kim minho <klaus9267@gmail.com>
2024-06-23 15:11:12 +0900

    18 emit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
   <div class="container">
     <h1>To-Do List</h1>
-    <TodoSimpleForm></TodoSimpleForm>
+    <TodoSimpleForm @add-todo="addTodo"/>
     <div v-if="!todos.length" class="alert alert-danger p-2 mt-2">
       추가된 Todo가 없습니다
     </div>
@@ -13,17 +13,14 @@
           <label
               class="form-check-label"
               :class="{ todo: todo.completed }"
-          >
-
-            {{ todo.subject }}
+          >{{ todo.subject }}
           </label>
         </div>
         <div>
           <button
               class="btn btn-danger btn-sm"
               @click="deleteTodo(index)"
-          >
-            Delete
+          >Delete
           </button>
         </div>
       </div>
@@ -38,43 +35,29 @@ import {ref} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
-  components:{
+  components: {
     TodoSimpleForm,
   },
   setup() {
-    const todo = ref('');
     const todos = ref([]);
     // const reactive1 = reactive({}) reactive 는 arr , object를 설정할 때 사용 나머지는 ref
-    const hasError = ref(false);
-    const todoStyle = {
-      textDecoration: 'line-through',
-      color: 'gray',
-    }
+    // const todoStyle = {
+    //   textDecoration: 'line-through',
+    //   color: 'gray',
+    // }
 
-    const onSubmit = () => {
-      if (todo.value === '') {
-        hasError.value = true;
-      } else {
-        hasError.value = false;
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false,
-        });
-        todo.value = '';
-      }
-    };
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    }
 
     const deleteTodo = (index) => {
       todos.value.splice(index, 1);
     }
 
     return {
-      todo,
       todos,
-      onSubmit,
-      hasError,
-      todoStyle,
+      addTodo,
+      // todoStyle,
       deleteTodo,
     };
   },
