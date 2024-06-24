4e2e6ac59354809884c9705cb65afea0949f239f
Kim minho <klaus9267@gmail.com>
2024-06-25 00:59:46 +0900

    24 검색 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
 
   <div class="container">
-    <h4>count: {{count}}</h4>
-    <h4>double count computed: {{doubleCount}}</h4>
-    <h4>double count method: {{doubleCountMethod()}}</h4>
-    <button @click="count++">Add One</button>
     <h1>To-Do List</h1>
+    <input
+        class="form-control"
+        type="text"
+        v-model="searchText"
+        placeholder="Search"
+    />
+    <hr>
     <TodoSimpleForm @add-todo="addTodo"/>
     <div v-if="!todos.length" class="alert alert-danger p-2 mt-2">
       추가된 Todo가 없습니다
     </div>
+    <div v-if="!filteredTodos.length && todos.length" class="alert alert-danger p-2 mt-2">
+      There is nothing to display
+    </div>
     <TodoList
-        :todos="todos"
+        :todos="filteredTodos"
         @toggle-todo="toggleTodo"
         @delete-todo="deleteTodo"
     />
@@ -50,23 +56,23 @@ export default {
       todos.value.splice(index, 1);
     }
 
-    const count = ref(1);
-    const doubleCount = computed(() => {
-      return count.value * 2;
+    const searchText = ref('')
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        })
+      }
+      return todos.value;
     })
-
-    const doubleCountMethod = () => {
-      return count.value * 2;
-    }
 
     return {
       todos,
       addTodo,
       deleteTodo,
       toggleTodo,
-      count,
-      doubleCount,
-      doubleCountMethod,
+      searchText,
+      filteredTodos,
     };
   },
 };
