a3d232cef21f7774732dec6641068731fe7d5edb
Kim minho <klaus9267@gmail.com>
2024-06-26 18:43:42 +0900

    45 To-Do 수정 페이지 route 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "@/pages/Home.vue";
 import Todos from "@/pages/todos/Todos.vue";
+import Todo from "@/pages/todos/Todo.vue";
 
 const index = createRouter({
     history: createWebHistory(),
@@ -14,7 +15,12 @@ const index = createRouter({
             path: '/todos',
             name: 'Todos',
             component: Todos,
-        }
+        },
+        {
+            path: '/todos/:id',
+            name: 'Todo',
+            component: Todo,
+        },
     ]
 });
 
