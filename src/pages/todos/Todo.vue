393e789564f155f6015787d12489878515c85cde
Kim minho <klaus9267@gmail.com>
2024-06-27 00:22:32 +0900

    49 To-Do 페이지 컴포넌트 만들기 2

--- src/pages/todos/Todo.vue
+++ src/pages/todos/Todo.vue
@@ -4,23 +4,58 @@
     Loading...
   </div>
   <form v-else>
-    <div class="form-group">
-      <label>Todo Subject</label>
-      <input v-model="todo.subject" type="text" class="form-control">
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input
+              v-model="todo.subject"
+              type="text"
+              class="form-control"
+          >
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button
+                type="button"
+                class="btn"
+                :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                @click="toggleTodoStatus"
+            >
+              {{ todo.completed ? 'Complete' : 'Incomplete' }}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <button class="btn btn-primary">Save</button>
+
+
+    <button type="submit" class="btn btn-primary">
+      Save
+    </button>
+    <button
+        class="btn btn-outline-dark ml-2"
+        @click="moveToTodoListPage"
+    >
+      Cancel
+    </button>
   </form>
 </template>
 
 <script>
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import axios from "axios";
 import {ref} from "vue";
+import {routes} from "@/router";
 
 export default {
   setup() {
     const url = process.env.VUE_APP_DB;
     const route = useRoute();
+    const router = useRouter();
     const todo = ref(null);
     const loading = ref(true);
 
@@ -32,9 +67,21 @@ export default {
 
     getTodo();
 
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed;
+    };
+
+    const moveToTodoListPage = () => {
+      router.push({
+        name: routes.todos.name,
+      });
+    };
+
     return {
       todo,
       loading,
+      toggleTodoStatus,
+      moveToTodoListPage,
     };
   }
 }
